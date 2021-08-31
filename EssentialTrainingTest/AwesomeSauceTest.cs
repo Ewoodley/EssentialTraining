c9e57c20386c54d1cfbf0f3384f84033340ea2bb
Eric Woodley <noctisumbra@gmail.com>
2021-08-31 07:23:17 -0500

    Initial Commit

---
+++ EssentialTrainingTest/AwesomeSauceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EssentialTraining;
+
+namespace EssentialTrainingTest
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("Tobasco");
+            testInstance.Sauces.Add("Cholula");
+            testInstance.Sauces.Add("Trailer Trash");
+
+            //expect to pass
+            Assert.IsTrue(testInstance.IsSauceAwesome("Trailer Trash"));
+
+            //expect false
+            Assert.IsFalse(testInstance.IsSauceAwesome("Ketsup"));
+
+        }
+    }
+}
---
+++ EssentialTrainingTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTrainingTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialTrainingTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ba60dec-6084-45ad-ac34-895b18c206c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
